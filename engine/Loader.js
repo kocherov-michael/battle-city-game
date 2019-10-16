b8790e8f96e1e8671030c0d101d0173f7834f748
Michael <ko4erov@mail.ru>
2019-10-16 11:53:29 +0300

    first commit

---
+++ engine/Loader.js
@@ -0,0 +1,101 @@
+;(function () {
+	'use strict'
+
+	
+	class Loader {
+		constructor () {
+			this.loadOrder = {
+				images: [],
+				jsons: []
+			}
+			this.resources = {
+				images: [],
+				jsons: []
+			}
+		}
+		// метод добавляет в очередь на загрузку
+		// метод принадлежит экзампляру класса
+		addImage (name, src) {
+			this.loadOrder.images.push({ name, src })
+		}
+
+		addJson (name, address) {
+			this.loadOrder.jsons.push({ name, address })
+		}
+
+		// загрузка изображений
+		load (callback) {
+			const promises = []
+
+			// проходим все изображения в очереда загрузки
+			for (const imageData of this.loadOrder.images) {
+				const { name, src } = imageData
+
+				const promise = Loader
+					.loadImage(src)
+					.then(image => {
+						// добавляем изображения в базу
+						this.resources.images[name] = image
+
+						// удаляем изображение из очереди загрузки
+						if (this.loadOrder.images.includes(imageData)) {
+							const index = this.loadOrder.images.indexOf(imageData)
+							this.loadOrder.images.splice(index,1)
+						}
+					})
+				promises.push(promise)
+			}
+
+			// проходим все json в очереда загрузки
+			for (const jsonData of this.loadOrder.jsons) {
+				const { name, address } = jsonData
+
+				const promise = Loader
+					.loadJson(address)
+					.then(image => {
+						// добавляем json в базу
+						this.resources.jsons[name] = image
+
+						// удаляем json из очереди загрузки
+						if (this.loadOrder.jsons.includes(jsonData)) {
+							const index = this.loadOrder.jsons.indexOf(jsonData)
+							this.loadOrder.jsons.splice(index,1)
+						}
+					})
+				promises.push(promise)
+			}
+			// ждём выполнения всех промисов
+			Promise.all(promises).then(callback)
+		}
+
+		// метод загрузки изображения
+		// static - метод принадлежит классу
+		static loadImage (src) {
+			return new Promise((resolve, reject) => {
+				try {
+					const image = new Image
+					image.onload = () => resolve(image)
+					image.src = src
+				}
+
+				catch (err) {
+					reject(err)
+				}
+			})
+		}
+
+		// метод загрузки json
+		static loadJson (address) {
+			return new Promise((resolve, reject) => {
+				fetch(address)
+					.then(result => result.json())
+					.then(result => resolve(result))
+					.catch(err => reject(err))
+			})
+		}
+	}
+	
+	// передаём класс в глобальный доступ
+	window.GameEngine = window.GameEngine || {}
+	window.GameEngine.Loader = Loader
+})();
