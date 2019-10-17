5e2b4c3ad906291957b51428b342684242665c8f
Michael <ko4erov@mail.ru>
2019-10-18 02:38:34 +0300

    add tanks moving

--- engine/Sprite.js
+++ engine/Sprite.js
@@ -26,65 +26,8 @@
 				this.height = this.frame.height
 			}
 
-			// this.x = args.x || 0
-			// this.y = args.y || 0
-			// // задаём координаты якоря изображения
-			// this.anchorX = args.anchorX || 0
-			// this.anchorY = args.anchorY || 0
-			// this.width = args.width || this. frame.width
-			// this.height =  args.height || this.frame.height
-
-			// if (args.scale !== undefined) {
-			// 	this.setScale(args.scale)
-			// }
 		}
 
-		// устанавливаем масштаб для обоих осей
-		// setScale (value) {
-		// 	this.scaleX = value
-		// 	this.scaleY = value
-		// }
-
-		// получаем абсолютные значения положения изображения
-		// для позиционирования по якорю
-		// get absoluteX () {
-		// 	return this.x - this.anchorX * this.width
-		// }
-
-		// set absoluteX (value) {
-		// 	this.x = value + this.anchorX * this.width
-		// 	return value
-		// }
-		
-		// get absoluteY () {
-		// 	return this.y - this.anchorY * this.height
-		// }
-		
-		// set absoluteY (value) {
-		// 	this.y = value + this.anchorY * this.height
-		// 	return value
-		// }
-
-		// задаём геттеры и сеттеры (функции, могут вычисляться на лету)
-		// get scaleX () {
-		// 	return this.width / this.frame.width
-		// }
-
-		// set scaleX (value) {
-		// 	this.width = this.frame.width * value
-		// 	return value
-		// }
-
-		// get scaleY () {
-		// 	return this.height / this.frame.height
-		// }
-
-		// set scaleY (value) {
-		// 	this.height = this.frame.height * value
-		// 	return value
-		// }
-
-		
 		draw ( canvas, context) {
 			context.save()
 			context.translate(this.x, this.y)
