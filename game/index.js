8dae85abb93db2ffa4fce127481b2468d848045f
Michael <ko4erov@mail.ru>
2019-10-20 00:21:57 +0300

    bubble move ready

--- game/index.js
+++ game/index.js
@@ -67,106 +67,64 @@ const mainScene = new Scene({
 		const { keyboard } = this.parent
 		// console.log(this)
 		// console.log(this.point.speedMove)
-		this.point.timeMemoryBack = this.point.timeMemoryBack || {left: 0}
-		this.point.timeMemoryForward = this.point.timeMemoryForward || {left: 0}
-		this.point.speedMoveForward = this.point.speedMoveForward || {left: 0}
-		this.point.speedMove = this.point.speedMove || {left: 0}
+		this.point.timeMemoryBack = this.point.timeMemoryBack || {left: 0, right: 0, up: 0, down: 0}
+		this.point.timeMemoryForward = this.point.timeMemoryForward || {left: 0, right: 0, up: 0, down: 0}
+		this.point.speedMoveForward = this.point.speedMoveForward || {left: 0, right: 0, up: 0, down: 0}
+		this.point.speedMoveBack = this.point.speedMoveBack || {left: 0, right: 0, up: 0, down: 0}
+		this.point.speedMove = this.point.speedMove || {left: 0, right: 0, up: 0, down: 0}
+		this.point.speedMoveForwardBack = this.point.speedMoveForwardBack || {left: 0, right: 0, up: 0, down: 0}
+		this.point.startSpeed = this.point.startSpeed || {left: 0, right: 0, up: 0, down: 0}
 
 		
 		let speedRotation = keyboard.space ? Math.PI / 100 : Math.PI / 200
 		// let pointSpeedDown = keyboard.arrowDown ? 
 		// this.point.timeMemory = this.point.timestamp || timestamp
 		// this.point.speedMoveDown = timestamp - this.point.timeMemory
-		// function f(obj) {
-		// 	console.log(obj)
-		// }
-		// f(this)
 		
-        if (keyboard.arrowUp) {
-			this.point.timeMemoryUpBack = null
-			this.point.timeMemoryUpForward = this.point.timeMemoryUpForward || timestamp
-			this.point.speedMoveUpForward = timestamp - this.point.timeMemoryUpForward
-			this.point.speedMoveUp = this.point.speedMoveUpForward / 500
-			this.point.y -= this.point.speedMoveUp
-        } else if (this.point.speedMoveUpForward && this.point.speedMoveUp > 0) {
-			this.point.timeMemoryUpForward = null
-			this.point.timeMemoryUpBack = this.point.timeMemoryUpBack || timestamp
-			this.point.speedMoveUpBack = (this.point.speedMoveUp > 0) ? timestamp - this.point.timeMemoryUpBack : 0
-			this.point.speedMoveUp = (this.point.speedMoveUpForward - this.point.speedMoveUpBack) / 500
-			this.point.y -= this.point.speedMoveUp
+		if (this.point.y < 50) {
+			// this.point.y = 50
+			// this.point.speedMove.up *= -1
+			// console.log(this.point.speedMove.up)
+		}
+		
+		if (keyboard.arrowUp) {
+			this.point.y -= movePointForvard('up', this)
+		} else if (this.point.speedMoveForward.up && this.point.speedMove.up > 0) {
+			this.point.y -= movePointBack('up', this)
 		} 
 		
-        if (keyboard.arrowDown) {
-			this.point.timeMemoryDownBack = null
-			this.point.timeMemoryDownForward = this.point.timeMemoryDownForward || timestamp
-			this.point.speedMoveDownForward = timestamp - this.point.timeMemoryDownForward
-			this.point.speedMoveDown = this.point.speedMoveDownForward / 500
-			this.point.y += this.point.speedMoveDown
-        } else if (this.point.speedMoveDownForward && this.point.speedMoveDown > 0) {
-			this.point.timeMemoryDownForward = null
-			this.point.timeMemoryDownBack = this.point.timeMemoryDownBack || timestamp
-			this.point.speedMoveDownBack = (this.point.speedMoveDown > 0) ? timestamp - this.point.timeMemoryDownBack : 0
-			this.point.speedMoveDown = (this.point.speedMoveDownForward - this.point.speedMoveDownBack) / 500
-			this.point.y += this.point.speedMoveDown
+		if (keyboard.arrowDown) {
+			this.point.y += movePointForvard('down', this)
+		} else if (this.point.speedMoveForward.down && this.point.speedMove.down > 0) {
+			this.point.y += movePointBack('down', this)
 		} 
 
 		if (keyboard.arrowRight) {
-			// this.point.timeMemoryRightBack = null
-			// this.point.timeMemoryRightForward = this.point.timeMemoryRightForward || timestamp
-			// this.point.speedMoveRightForward = timestamp - this.point.timeMemoryRightForward
-			// this.point.speedMoveRight = this.point.speedMoveRightForward / 500
-			// this.point.x += this.point.speedMoveRight
 			this.point.x += movePointForvard('right', this)
-        } else if (this.point.speedMoveForward.right && this.point.speedMove.right > 0) {
-			// this.point.timeMemoryRightForward = null
-			// this.point.timeMemoryRightBack = this.point.timeMemoryRightBack || timestamp
-			// this.point.speedMoveRightBack = (this.point.speedMoveRight > 0) ? timestamp - this.point.timeMemoryRightBack : 0
-			// this.point.speedMoveRight = (this.point.speedMoveRightForward - this.point.speedMoveRightBack) / 500
-			// this.point.x += this.point.speedMoveRight
-			this.point.x += movePointBack ('right', this)
+		} else if (this.point.speedMoveForward.right && this.point.speedMove.right > 0) {
+			this.point.x += movePointBack('right', this)
 		} 
 
 		if (keyboard.arrowLeft) {
-			// this.point.timeMemoryLeftBack = null
-			// this.point.timeMemoryLeftForward = this.point.timeMemoryLeftForward || timestamp
-			// this.point.speedMoveLeftForward = timestamp - this.point.timeMemoryLeftForward
-			// this.point.speedMoveLeft = this.point.speedMoveLeftForward / 500
-			// this.point.x -= this.point.speedMoveLeft
 			this.point.x -= movePointForvard('left', this)
-			// this.point.x -= moveforvard.bind(this)
-        } else if (this.point.speedMoveForward.left && this.point.speedMove.left > 0) {
-			// this.point.timeMemoryLeftForward = null
-			// this.point.timeMemoryLeftBack = this.point.timeMemoryLeftBack || timestamp
-			// this.point.speedMoveLeftBack = (this.point.speedMoveLeft > 0) ? timestamp - this.point.timeMemoryLeftBack : 0
-			// this.point.speedMoveLeft = (this.point.speedMoveLeftForward - this.point.speedMoveLeftBack) / 500
-			// this.point.x -= this.point.speedMoveLeft
-			this.point.x -= movePointBack ('left', this)
+		} else if (this.point.speedMoveForward.left && this.point.speedMove.left > 0) {
+			this.point.x -= movePointBack('left', this)
 		} 
 
-		// this.point.timeMemoryBack = this.point.timeMemoryBack || {}
-		// this.point.timeMemoryForward = this.point.timeMemoryForward || {}
-		// this.point.speedMoveForward = this.point.speedMoveForward || {}
-		// this.point.speedMove = this.point.speedMove || {}
-
-		// const moveforvard = movePointForvard('left')
 		function movePointForvard (direction, scene) {
-			// const obj = {speedMoveForward }
-			// console.log(obj.point)
 			scene.point.timeMemoryBack[direction] = null
 			scene.point.timeMemoryForward[direction] = scene.point.timeMemoryForward[direction] || timestamp
-			scene.point.speedMoveForward[direction] = timestamp - scene.point.timeMemoryForward[direction]
+			scene.point.speedMoveForward[direction] = timestamp - scene.point.timeMemoryForward[direction] + scene.point.startSpeed[direction] * 500
 			scene.point.speedMove[direction] = scene.point.speedMoveForward[direction] / 500
 			return scene.point.speedMove[direction]
 		}
 
 		function movePointBack (direction, scene) {
-			// const obj = {speedMoveForward }
 			scene.point.timeMemoryForward[direction] = null
 			scene.point.timeMemoryBack[direction] = scene.point.timeMemoryBack[direction] || timestamp
-			// obj.point.speedMoveBack[direction] = (obj.point.speedMove[direction] > 0) ? timestamp - scene.point.timeMemoryBack[direction] : 0
-			// return (scene.point.speedMoveForward[direction] - ((obj.point.speedMove[direction] > 0) ? timestamp - scene.point.timeMemoryBack[direction] : 0)) / 500
-			// obj.point.speedMove[direction] = (scene.point.speedMoveForward[direction] - obj.point.speedMoveBack[direction]) / 500
-			scene.point.speedMove[direction] = (scene.point.speedMoveForward[direction] - ((scene.point.speedMove[direction] > 0) ? timestamp - scene.point.timeMemoryBack[direction] : 0)) / 500
+			scene.point.speedMoveBack[direction] = scene.point.speedMove[direction] > 0 ? timestamp - scene.point.timeMemoryBack[direction] : 0
+			scene.point.speedMove[direction] = (scene.point.speedMoveForward[direction] - scene.point.speedMoveBack[direction]) / 500
+			scene.point.startSpeed[direction] = scene.point.speedMove[direction]
 			return scene.point.speedMove[direction]
 		}
 
